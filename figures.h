e7b4db8ed7b9333441fd59bf64badeecb283a4dd
Dan <d.safonov.2007@mail.ru>
2026-03-19 20:16:42 +0700

    add main.cpp figures.h circle.h circle.cpp

---
+++ figures.h
@@ -0,0 +1,9 @@
+#ifndef FIGURES_H
+#define FIGURES_H
+enum figures {
+	RECTANGLE = 1,
+	TRIANGLE,
+	CIRCLE,
+	TRAPEZOID,
+};
+#endif // FIGURES_H
---
+++ main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include "figures.h"
+//#include "math.h"
+#include "circle.h"
+//#include "ractangle.h"
+//#include "trapezoid.h"
+
+int main() {
+	std::cout << "1-Прямоугольник" << std::endl
+	<< "2-Треугольник" << std::endl
+	<< "3-Окружность" << std::endl
+	<< "4-Трапеция" << std::endl;
+
+	int input;
+	std::cin >> input;
+
+	switch (input) {
+	case figures::RECTANGLE:
+//		ractangle();
+		break;
+	case figures::TRIANGLE:
+//		math::testTriangle();
+		break;
+	case figures::CIRCLE:
+		circle();
+		break;
+	case figures::TRAPEZOID:
+//		trapezoid();
+		break;
+	default:
+		std::cout << "Ошибка нужны цифры от 1 до 4" << std::endl;
+		}
+	return 0;
+	}

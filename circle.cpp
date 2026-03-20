ffba4905472c1ca31b8cff3cc6d165506ea9e066
grisha <birgregory2007@gmail.com>
2026-03-20 09:09:11 +0700

    fixed

--- circle.cpp
+++ circle.cpp
@@ -8,11 +8,11 @@ void circle(){
 	float pi = 3.14;
 	std::cout << "Радиус окружности и угол для поиска прощади сектора"
 	<< std::endl;
-	if ((std::cin >> radius && radius > 0) && (std::cin >> corner && corner > -1)) {
+	if ((std::cin >> radius && radius > 0) && (std::cin >> corner && corner > -1 && corner < 361)) {
 	std::cout << std::fixed << std::setprecision(2) << "Длина окружности - " << 2 * pi *radius << std::endl;
 	std::cout << std::fixed << std::setprecision(2) << "Площадь круга - " << pi * radius * radius << std::endl;
 	std::cout << std::fixed << std::setprecision(2) << "Площадь кругового сектора - " << pi * radius * radius * corner / 360 << std::endl;
 	} else {
-	std::cout << "Ошибка радиус больше 0 и угол от 0 градусов" << std::endl;
+	std::cout << "Ошибка радиус больше 0 и угол от 0 градусов и до 360 градусов" << std::endl;
 		}
 }
